8c3991f8f6e40b4f95cb44f31abbdceee3083b70
koosco <rnxogud123@naver.com>
2024-10-15 00:58:00 +0900

    refactor : Theater의 판매 책임을 TicketSeller가 하도록 변경
    * TicketSeller가 자율적으로 Audience에게 판매하도록 코드 수정
    * 외부에서 TicketOffice에 접근할 수 없게 캡슐화

--- src/main/java/object/TicketSeller.java
+++ src/main/java/object/TicketSeller.java
@@ -3,8 +3,19 @@ package object;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public class TicketSeller {
     private TicketOffice ticketOffice;
+
+    public void sellTo(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketOffice.getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketOffice.plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
 }
