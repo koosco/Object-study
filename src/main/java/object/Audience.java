31952eba3da54ef66bad43e7bf306fb949f615eb
koosco <rnxogud123@naver.com>
2024-10-15 01:09:01 +0900

    refactor : Audience가 초대장이 있는지 확인하도록 책임 분리
    * TicketSeller가 Audience의 가방에 접근할 수 없도록 Audience를 캡슐화
    * Audience가 Bag을 직접 처리하기 때문에 외부에서는 Audience가 Bag을 소유하고 있는 것을 알 수 없다

--- src/main/java/object/Audience.java
+++ src/main/java/object/Audience.java
@@ -1,11 +1,20 @@
 package object;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@Getter
 @AllArgsConstructor
 public class Audience {
 
     private Bag bag;
+
+    public Long buy(Ticket ticket) {
+        if (bag.hasInvitation()) {
+            bag.setTicket(ticket);
+            return 0L;
+        } else {
+            bag.setTicket(ticket);
+            bag.minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
 }
